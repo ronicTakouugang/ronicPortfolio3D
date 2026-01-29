c6dea73750b2867d82cf211ed0c1f49e3d95b77e
TAKOUGANG RONIC <ronic_tk@MacBook-Air-de-TAKOUGANG.local>
2026-01-29 13:43:31 +0100

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <h1>Bienvenue sur mon Portfolio 3D</h1>
+            <p>Si vous voyez ce message, l'application fonctionne.</p>
+        </main>
+    )
+}
+export default App
